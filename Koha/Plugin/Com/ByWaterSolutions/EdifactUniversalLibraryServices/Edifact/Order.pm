c227f3e570ef40e2d62e743869399f4cf0ea2cea
Kyle M Hall <kyle@bywatersolutions.com>
2026-05-06 18:11:48 -0400

    Universal Library Services - Update files [Vendor]

--- Koha/Plugin/Com/ByWaterSolutions/EdifactUniversalLibraryServices/Edifact/Order.pm
+++ Koha/Plugin/Com/ByWaterSolutions/EdifactUniversalLibraryServices/Edifact/Order.pm
@@ -1,4 +1,4 @@
-package Koha::Plugin::Com::ByWaterSolutions::EdifactEnhanced::Edifact::Order;
+package Koha::Plugin::Com::ByWaterSolutions::EdifactUniversalLibraryServices::Edifact::Order;
 
 use Modern::Perl;
 use utf8;
